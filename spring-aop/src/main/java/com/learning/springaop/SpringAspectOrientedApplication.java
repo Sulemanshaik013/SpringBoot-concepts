c6975c394fd116ef5a4c2bacb7b050879df47683
Sulemanshaik013 <Sulemanshaik013@gmail.com>
2024-02-10 13:25:01 +0530

    initial commit: spring-aop demo

---
+++ spring-aop/src/main/java/com/learning/springaop/SpringAspectOrientedApplication.java
@@ -0,0 +1,15 @@
+package com.learning.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringAspectOrientedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAspectOrientedApplication.class, args);
+	}
+
+}
